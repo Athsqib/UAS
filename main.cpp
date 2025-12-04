7de6b3c98da125073bdfa47e5c369d0c6cc3e839
Athsqib <athar.ts.a@gmail.com>
2025-12-04 21:02:28 +0700

    Implement shop system and more fix for the map (loop gameplay still broken)

--- main.cpp
+++ main.cpp
@@ -88,9 +88,10 @@ char cell(int y, int x){
 
 struct player {
     int playerX, playerY;
-    int money = 0;
+    int money = 8000;
     int mailAmount;
-    int stamPlayer = 100;
+    int stamPlayer = 30;
+    int maxStamPlayer = 100;
     bool defend;
 } player;
 
@@ -98,19 +99,62 @@ struct pencuri {
     int thiefX, thiefY;
     int amount;
     int stamThief = 20;
+    int maxStamThief = 20;
+    int chance = 20;
+    int fullAmount = 50;
+    int effectiveness = 5;
     bool active;
 } pencuri;
 
 struct mail {
     int mailX, mailY;
     int amount;
+    int fullAmount = 100;
+    int effectiveness = 10;
     bool taken;
 } mailItem;
 
-int mailSpawned = 0;
-int maxMail;
+struct staminaItems {
+    string name;
+    int price;
+    string description;
+    int staminaRestore;
+    int stockAmount;
+    int stockEffectiveness;
+} staminaItem;
 
-int playerX = 0, playerY = 0;
+struct shopUpgrade {
+    string name;
+    int price;
+    string description;
+    int level;
+    int stock;
+} shopUpgradeItem;
+
+vector<staminaItems> shopStaminaItems = {
+    {"Air Kelapa", 50, "Kaya akan elektrolit alami seperti kalium untuk mengganti cairan tubuh yang hilang saat beraktivitas fisik yang dapat menambahkan stamina mu sebanyak 30 poin.", 30, 30, 10},
+    {"Kopi", 150, "Minuman hitam pait yang penuh kafein, dapat menambah stamina mu sebanyak 60 poin.", 60, rand() % 20 + 5},
+    {"Smoothie Piscang", 300, "Smoothie dengan rasa campuran dari pisang dan selai kacang yang dapat menambah stamina mu sebanyak 100 poin.", 100, 10, 1},
+    {"Blue Bull", 500, "Minuman energi yang mengandung kafein, taurine, dan vitamin B yang dapat menambah stamina mu sebanyak 150 poin.", 150, 5, 1},
+    {"Expresso 10 Shot", 1000, "Minuman kopi yang sangat kuat dengan 10 shot espresso yang dapat menambah stamina mu sebanyak 300 poin.", 300, 3, 1}
+};
+
+vector<shopUpgrade> shopUpgradeItems = {
+    {"Stamina Upgrade", 1000, "Meningkatkan kapasitas stamina mu sebanyak 50 poin.", 0, 5},
+    {"Mail Spawn Upgrade", 1500, "Meningkatkan jumlah maksimal paket yang dapat diambil sebanyak 2.", 0, 5},
+    {"Mail Amount Upgrade", 2000, "Meningkatkan jumlah uang yang didapat dari setiap paket sebanyak 50 rupiah.", 0, 10},
+    {"Mail Effectiveness Upgrade", 2000, "Meningkatkan peluang mendapatkan paket bernilai tinggi sebanyak 15%.", 0, 5},
+    {"Thief Spawn Upgrade", 2500, "Meningkatkan peluang munculnya maling sebanyak 10%.", 0, 5},
+    {"Thief Amount Upgrade", 3000, "Meningkatkan jumlah uang yang didapat dari maling sebanyak 20 rupiah.", 0, 10},
+    {"Thief Effectiveness Upgrade", 3000, "Meningkatkan efektivitas maling dalam mencuri paketmu sebanyak 10%.", 0, 5}
+};
+
+vector<int> actualStaminaStock;
+
+int mailSpawned = 0;
+int maxMail = 4;
+
+int playerX, playerY;
 
 void wait(){
     this_thread::sleep_for(1s);
@@ -136,6 +180,14 @@ int waitNewKey(){
         bool z = GetAsyncKeyState('Z') & 0x8000 || GetAsyncKeyState('z') & 0x8000;
         bool x = GetAsyncKeyState('X') & 0x8000 || GetAsyncKeyState('x') & 0x8000;
         bool c = GetAsyncKeyState('C') & 0x8000 || GetAsyncKeyState('c') & 0x8000;
+        bool y = GetAsyncKeyState('Y') & 0x8000 || GetAsyncKeyState('y') & 0x8000;
+        bool n = GetAsyncKeyState('N') & 0x8000 || GetAsyncKeyState('n') & 0x8000;
+        bool num0 = GetAsyncKeyState('0') & 0x8000;
+        bool num1 = GetAsyncKeyState('1') & 0x8000;
+        bool num2 = GetAsyncKeyState('2') & 0x8000;
+        bool num3 = GetAsyncKeyState('3') & 0x8000;
+        bool num4 = GetAsyncKeyState('4') & 0x8000;
+        
 
         int key = 0;
 
@@ -147,6 +199,13 @@ int waitNewKey(){
         else if (z) key = 'z';
         else if (x) key = 'x';
         else if (c) key = 'c';
+        else if (y) key = 'y';
+        else if (n) key = 'n';
+        else if (num0) key = '0';
+        else if (num1) key = '1';
+        else if (num2) key = '2';
+        else if (num3) key = '3';
+        else if (num4) key = '4';
         else key = 0;
 
         if (key && !keyWasDown){
@@ -162,8 +221,8 @@ int waitNewKey(){
 
 void playerPlacement(){
     bool placed = false;
-    for (int y = 0; y < tinggi && !placed; ++y) {
-        for (int x = 0; x < lebar; ++x) {
+    for (int y = 0; y < tinggi && !placed; y++) {
+        for (int x = 0; x < lebar; x++) {
             if ((*currentMap)[y][x] == '*') {
                 playerX = x;
                 playerY = y;
@@ -221,18 +280,30 @@ void printMaps(){
 
 void trySpawnThief(){
     int chance = rand() % 100;
-    if (chance < 20) {
+    if (chance < pencuri.chance){
         int tries = 0;
         do{
             pencuri.thiefX = std::rand() % (lebar - 2) + 1;
             pencuri.thiefY = std::rand() % (tinggi - 2) + 1;
-        } while (cell(pencuri.thiefY, pencuri.thiefX) != '*' && ++tries < 200);
+        } while (cell(pencuri.thiefY, pencuri.thiefX) != '*' && tries++ < 200);
         pencuri.active = true;
+        pencuri.stamThief = 20;
         cout << "\nAda maling disekitar mu. Hati hati dalam perjalanannya.\n";
         wait();
     }
-    else {
+    else{
         pencuri.active = false;
+    }
+}
+
+void matiThief(){
+    if (pencuri.stamThief <= 0){
+        pencuri.amount = rand() % pencuri.fullAmount + pencuri.effectiveness;
+        cout << "Kamu berhasil mengelabui maling tersebut! Kamu mendapatkan " << pencuri.amount << " rupiah!\n";
+        wait();
+        player.money += pencuri.amount;
+        pencuri.active = false;
+        return;
     }
 }
 
@@ -280,16 +351,6 @@ void menyerangThief(){
     pencuri.stamThief -= damage;
     cout << "Kamu menyerang maling (-" << damage << " stamina maling)\n";
     wait();
-
-    if (pencuri.stamThief <= 0){
-        int drop = pencuri.amount;
-        drop = rand() % 30 + 1;
-        cout << "Kamu berhasil mengelabui maling tersebut! Kamu mendapatkan " << drop << " rupiah!\n";
-        wait();
-        player.money += drop;
-        pencuri.active = false;
-        return;
-    }
     giliranThief();
 }   
 
@@ -306,6 +367,7 @@ void kaburThief(){
         giliranThief();
     }
 }
+
 void interactThief(){
     if (!pencuri.active) return;
     if (playerX != pencuri.thiefX || playerY != pencuri.thiefY) return;
@@ -314,9 +376,9 @@ void interactThief(){
         printMaps();
         cout << "\nKamu bertemu dengan maling, bertahan sampai maling itu kelelahan!!\n";
         cout << "1. Menyerang (Z)\n2. Bertahan (X)\n3. Kabur (C)\n";
-        cout << "Stamina mu = " << player.stamPlayer << endl;
-        cout << "Stamia maling = " << pencuri.stamThief << endl;
-        cout << "Pilihamnu = (Z/X/C)";
+        cout << "Stamina mu = " << player.stamPlayer << "/" << player.maxStamPlayer << endl;
+        cout << "Stamina maling = " << pencuri.stamThief << "/" << pencuri.maxStamThief << endl;
+        cout << "Pilihamnu = (Z/X/C)\n";
         flushInput();
             
         switch (waitNewKey()) {
@@ -337,17 +399,35 @@ void interactThief(){
     }
     system("cls||clear");
     printMaps();          
-    
-    if (pencuri.stamThief <= 0){
-        cout << "Maling itu kelelahan dan pergi!\n";
-        waitLong();
-        pencuri.active = false;
-    }
-    else if (player.stamPlayer <= 0){
+    matiThief();
+    if (player.stamPlayer <= 10){
         cout << "Kamu kelelahan dan jatuh pingsan! Maling itu mengambil paketmu dan pergi...\n";
         waitLong();
-        maxMail--;
+        int randomLoss = rand() % mailItem.amount + 10;
+        if (mailSpawned = maxMail) player.money -= randomLoss;
+        else maxMail--;
         pencuri.active = false;
+    }
+}
+
+void moveThiefTowardPlayer(){
+    if (!pencuri.active) return;
+
+    int dx = 0;
+    int dy = 0;
+
+    if(pencuri.thiefX < playerX) dx = 1;
+    else if(pencuri.thiefX > playerX) dx = -1;
+
+    if(pencuri.thiefY < playerY) dy = 1;
+    else if(pencuri.thiefY > playerY) dy = -1;
+
+    int newThiefX = pencuri.thiefX + dx;
+    int newThiefY = pencuri.thiefY + dy;
+
+    if (newThiefX >= 0 && newThiefX < lebar && newThiefY >= 0 && newThiefY < tinggi && cell(newThiefY, newThiefX) == '*') {
+        pencuri.thiefX = newThiefX; 
+        pencuri.thiefY = newThiefY;
     }
 }
 
@@ -362,7 +442,7 @@ void trySpawnMail(){
         percobaan++;
     } while (cell(mailItem.mailY, mailItem.mailX) != '*' && percobaan < 200);
 
-    mailItem.amount = rand() % 100 + 30;
+    mailItem.amount = rand() % mailItem.fullAmount + mailItem.effectiveness;
     mailItem.taken = false;
 
     mailSpawned++;
@@ -416,6 +496,7 @@ void map(){
             (*currentMap)[playerY][playerX] = '*';
             playerX = newX;
             playerY = newY;
+            moveThiefTowardPlayer();
             (*currentMap)[playerY][playerX] = 'P';
             
             checkPickup();
@@ -425,15 +506,250 @@ void map(){
             printMaps();
             cout << "Bergerak (WASD) Tidak dapat di hold.";
             cout << "\nUang = " << player.money << " rupiah\n";
-            cout << "Stamina = " << player.stamPlayer << endl;
+            cout << "Stamina = " << player.stamPlayer << "/" << player.maxStamPlayer << endl;
             cout << "Lokasi paket = " << mailSpawned << "/" << maxMail << endl;
         };
     }
 }
 
+void notEnoughMoney(int punya, int butuh){
+    cout << "Uang mu tidak cukup! Kamu memiliki " << punya << " rupiah, tetapi butuh " << butuh << " rupiah.\n";
+    wait();
+}
+
+void upgradeStaminaCapacity(int){
+    player.maxStamPlayer += 50;
+} 
+
+void upgradeMailSpawnAmount(int){
+    maxMail += 2;
+}
+
+void upgradeMailAmount(int){
+    mailItem.fullAmount += 50;
+}
+
+void upgradeMailEffectiveness(int){
+    mailItem.effectiveness += ((15/100) * mailItem.effectiveness);
+}
+
+void upgradeThiefSpawnChance(int){
+    pencuri.chance += 10;
+}
+
+void upgradeThiefAmount(int){
+    pencuri.fullAmount += 20;
+}
+
+void upgradeThiefEffectiveness(int){
+    pencuri.effectiveness += ((10/100) * pencuri.effectiveness);
+}
+
+void choiceShopStaminaItem(int index){
+    int stock = rand() % shopStaminaItems[index].stockAmount + shopStaminaItems[index].stockEffectiveness;
+    cout << shopStaminaItems[index].description << endl;
+    cout << "Apakah kamu yakin ingin membelinya? (Y/N)";
+    flushInput();
+    int confirmUp = waitNewKey();
+    if (confirmUp == 'y' || confirmUp == 'Y'){
+        if (player.money >= shopStaminaItems[index].price){
+            player.money -= shopStaminaItems[index].price;
+            player.stamPlayer += shopStaminaItems[index].staminaRestore;
+            cout << "\nKamu membeli " << shopStaminaItems[index].name << "!\nStamina mu bertambah " << shopStaminaItems[index].staminaRestore << " poin.\n";
+            waitLong();
+        }
+        else if (stock <= 0){
+            cout << "Maaf, stok " << shopStaminaItems[index].name << " sedang habis.\n";
+            wait();
+        }
+        else {
+            notEnoughMoney(player.money, shopStaminaItems[index].price);
+        }
+    }
+    else if (confirmUp == 'n' || confirmUp == 'N'){
+        cout << "Pembelian dibatalkan.\n";
+        wait();
+    }
+}
+
+void shopUpgradeRaisePrice(int index){
+    shopUpgradeItems[index].price += shopUpgradeItems[index].price / 2;
+}
+
+void choiceShopUpgradeItem(int index, void (*upgradeFunction)(int)){
+    cout << shopUpgradeItems[index].description << endl;
+    cout << "Apakah kamu yakin ingin meng-upgrade? (Y/N)";
+    flushInput();
+    int confirmUp = waitNewKey();
+    if (confirmUp == 'y' || confirmUp == 'Y'){
+        if (player.money >= shopUpgradeItems[index].price){
+            if (shopUpgradeItems[index].level >= shopUpgradeItems[index].stock){
+                cout << "Upgrade sudah mencapai level maksimal.\n";
+                wait();
+            }
+            else {
+                player.money -= shopUpgradeItems[index].price;
+                upgradeFunction(index);
+                shopUpgradeItems[index].level++;
+                cout << "\nKamu meng-upgrade " << shopUpgradeItems[index].name << "!";
+                shopUpgradeRaisePrice(index);
+                waitLong();
+            }
+        }
+        else {
+            notEnoughMoney(player.money, shopUpgradeItems[index].price);
+        }
+    }
+    else if (confirmUp == 'n' || confirmUp == 'N'){
+        cout << "Upgrade dibatalkan.\n";
+        wait();
+    }
+}
+
+void shop(){
+    bool upgradeShop = false;
+    bool move = false;
+    bool openshop = true;
+    bool inUpgradedShop = false;
+    while (!move && openshop){
+        system("cls||clear");
+        cout << "Selamat datang di toko!\n";
+        for (size_t i = 0; i < 3; i++){
+            int stock = rand() % shopStaminaItems[i].stockAmount + shopStaminaItems[i].stockEffectiveness;
+            cout << i + 1 << ". " << shopStaminaItems[i].name << " - " << shopStaminaItems[i].price << " rupiah" << " (Stok: " << stock << ")\n";
+        }
+        if (!upgradeShop) cout << "4. Upgrade Toko - 1000 rupiah";
+        else if (upgradeShop) {
+            cout << "4. Upgrade Toko (Sudah di-upgrade) - Pilih untuk masuk ke dalam toko yang sudah di upgrade";
+        }
+        cout << "\n0. Keluar dari toko\n";
+        cout << "Uang mu = " << player.money << " rupiah\n";
+        cout << "Stamina = " << player.stamPlayer << "/" << player.maxStamPlayer << endl;
+        cout << "Pilihanmu = ";
+        flushInput();
+        int choice = waitNewKey();
+        if (choice == '1'){
+            choiceShopStaminaItem(0);
+        }
+        else if (choice == '2'){
+            choiceShopStaminaItem(1);
+        }
+        else if (choice == '3'){
+            choiceShopStaminaItem(2);
+        }
+        else if (choice == '4'){
+            if (!upgradeShop){
+                cout << "Apakah kamu yakin ingin meng-upgrade toko? (Y/N)";
+                flushInput();
+                int confirm = waitNewKey();
+                if (confirm == 'y' || confirm == 'Y'){
+                    if (player.money >= 1000){
+                        player.money -= 1000;
+                        upgradeShop = true;
+                        cout << "\nToko berhasil di-upgrade! Lebih banyak item tersedia di toko!\n";
+                        waitLong();
+                    }
+                    else {
+                        notEnoughMoney(player.money, 1000);
+                    }
+                }
+                else if (confirm == 'n' || confirm == 'N'){
+                    cout << "Upgrade dibatalkan.\n";
+                    wait();
+                }
+            }
+            else {
+                cout << "Masuk ke dalam toko yang sudah di-upgrade? (Y/N)";
+                flushInput();
+                int confirm = waitNewKey();
+                if (confirm == 'y' || confirm == 'Y'){
+                    inUpgradedShop = true;
+                    while (inUpgradedShop){
+                        system("cls||clear");
+                        cout << "Toko yang sudah di-upgrade:";
+                        for (size_t i = 3; i < shopStaminaItems.size(); i++){
+                            cout << "\n" << i - 2 << ". " << shopStaminaItems[i].name << " - " << shopStaminaItems[i].price << " rupiah (Stok: " << actualStaminaStock[i] << ")";
+                        }
+                        for (size_t i = 0; i < shopUpgradeItems.size(); i++){
+                            cout << "\n" << i + 3 << ". " << shopUpgradeItems[i].name << " - " << shopUpgradeItems[i].price << " rupiah (Level: " << shopUpgradeItems[i].level << ")";
+                            if (shopUpgradeItems[i].level >= shopUpgradeItems[i].stock){
+                                cout << " (Sudah max)";
+                            }
+                        }
+                        cout << "0. Kembali ke toko utama\n";
+                        cout << "Uang mu = " << player.money << " rupiah\n";
+                        cout << "Stamina = " << player.stamPlayer << "/" << player.maxStamPlayer << endl;
+                        cout << "Pilihanmu = ";
+                        int choiceUp = waitNewKey();
+                        switch (choiceUp) {
+                            case '0':
+                                cout << "Kembali ke toko utama...\n";
+                                inUpgradedShop = false;
+                                break;
+                            case '1':
+                                choiceShopStaminaItem(3);
+                                break;
+                            case '2':
+                                choiceShopStaminaItem(4);
+                                break;
+                            case '3':
+                                choiceShopUpgradeItem(0, upgradeStaminaCapacity);
+                                break;
+                            case '4':
+                                choiceShopUpgradeItem(1, upgradeMailSpawnAmount);
+                                break;
+                            case '5':
+                                choiceShopUpgradeItem(2, upgradeMailAmount);
+                                break;
+                            case '6':
+                                choiceShopUpgradeItem(3, upgradeMailEffectiveness);
+                                break;
+                            case '7':
+                                choiceShopUpgradeItem(4, upgradeThiefSpawnChance);
+                                break;
+                            case '8':
+                                choiceShopUpgradeItem(5, upgradeThiefAmount);
+                                break;
+                            case '9':
+                                choiceShopUpgradeItem(6, upgradeThiefEffectiveness);
+                                break;
+                            default:
+                                cout << "Pilihan tidak valid. Silakan coba lagi.\n";
+                                wait();
+                                break;
+                            }
+                        }
+                    }
+                else if (confirm == 'n' || confirm == 'N'){
+                    cout << "Masuk ke bagian toko dibatalkan.\n";
+                    wait();
+                }
+                wait();
+            }
+        }
+        else if (choice == '0'){
+            cout << "Keluar dari toko...\n";
+            wait();
+            move = true;
+            openshop = false;
+            map();
+        }
+        else {
+            cout << "Pilihan tidak valid. Silakan coba lagi.\n";
+            wait();
+        
+        }
+    system("cls||clear");
+    }
+}
+
 int main(){
     srand(time(0));
-    maxMail = rand() % 5 + 2;
     currentMap = &allMaps[rand() % allMaps.size()];
-    map();
+    for (const auto& item : shopStaminaItems){
+        int stock = rand() % item.stockAmount + item.stockEffectiveness;
+        actualStaminaStock.push_back(stock);
+    }
+    shop();
+    
 }
